b489476d471521e630e3bc5f42bd3d02de8ffecf
Sergio Marchena <mar16387@uvg.edu.gt>
2019-08-28 12:17:30 -0600

    new changes
    new changes

--- testDeImportacion.R
+++ testDeImportacion.R
@@ -9,6 +9,7 @@
 # José Martínez			    15163
 # José Meneses		    	1514
 
+install.packages("frequency")
 #Librerias
 #Programa para juntar todos los xls en una tabla y exportar un RDATA
 library("readxl")
@@ -89,43 +90,43 @@ h1<-table(ventas.centroamerica$`Año Mes`)
 FRECventas2018<-h1[1:12]
 meses<-c("Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago","Sept", "Oct", "Nov","Dic")
 meses2<-c("Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul")
-barplot(FRECventas2018, main = "VENTAS EN 2018", names.arg = meses)
+barplot(FRECventas2018, main = "2018", names.arg = meses)
 FRECventas2019<-h1[13:19]
-barplot(FRECventas2019, main = "VENTAS EN 2019", names.arg = meses2)
-barplot(h1, main = "VENTAS TOTALES", names.arg = c(meses, meses2))
+barplot(FRECventas2019, main = "2019", names.arg = meses2)
+barplot(h1, main = "2018-2019", names.arg = c(meses, meses2))
 
 # PRODUCTO
-productos<-table(as.factor(ventas.centroamerica$Producto))
-str(productos)
-h1<-productos[order(-productos)]
-head(h1)
-xx<-barplot(head(h1, n = 10), names.arg = NA, horiz = FALSE, main = "PRODUCTOS MAS VENDIDOS")
-?barplot
-head(h1, n = 10)
-yy<-c(73,73,72,72,71,70,70,70,70,70)
-text(x = xx, y = yy, labels = yy, pos = 1, cex = 0.8, col = "red")
-# 4123800056 4123800080 4123800001 4123800030 4123811146 4123660214 4130000001 4130000002 4130000003 4130000004
-p1<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800056"][1]
-p2<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800080"][1]
-p3<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800001"][1]
-p4<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800030"][2]
-p5<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123811146"][1]
-p6<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123660214"][1]
-p7<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000001"][1]
-p8<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000002"][1]
-p9<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000003"][1]
-p10<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000004"][1]
-ps<-c(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10)
-ps
-axis(1, at=xx, labels=ps, tick=FALSE, las=2, line=-11, cex.axis=0.5)
-?barplot
+# productos<-table(as.factor(ventas.centroamerica$Producto))
+# str(productos)
+# h1<-productos[order(-productos)]
+# head(h1)
+# xx<-barplot(head(h1, n = 10), names.arg = NA, horiz = FALSE, main = "PRODUCTOS MAS PEDIDOS")
+# head(h1, n = 10)
+# yy<-c(73,73,72,72,71,70,70,70,70,70)
+# text(x = xx, y = yy, labels = yy, pos = 4, cex = 0.9, col = "red")
+# # 4123800056 4123800080 4123800001 4123800030 4123811146 4123660214 4130000001 4130000002 4130000003 4130000004
+# p1<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800056"]
+# p2<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800080"][1]
+# p3<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800001"][1]
+# p4<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800030"][2]
+# p5<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123811146"][1]
+# p6<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123660214"][1]
+# p7<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000001"][1]
+# p8<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000002"][1]
+# p9<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000003"][1]
+# p10<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000004"][1]
+# ps<-c(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10)
+# ps
+# p1
+# axis(1, at=xx, labels=ps, tick=FALSE, las=2, line=-11, cex.axis=0.5)
+
 
 # CODIGO CATEGORIA
 categorias<-table(ventas.centroamerica$Categoria)
 categorias
 h1<-categorias[order(-categorias)]
 h1
-barplot(h1)
+barplot(h1, main = "CATEGORIAS MAS PEDIDAS")
 
 #Tablas de frecuencia de todas las variables
 freq(ventas.centroamerica$`Año Mes`)
@@ -149,7 +150,7 @@ freq(ventas.centroamerica$`Recursos Especiales`)
 freq(ventas.centroamerica$`Treboles extra`)
 
 #Histograma de categoria pronostico
-hist(ventas.centroamerica$Pronostico, breaks=10, col="blue", xlab="Petal Length", main="Colored histogram")
+hist(ventas.centroamerica$Pronostico, breaks=10, col="blue", main="Colored histogram")
 
 # separación de variables numéricas de las categóricas
 num <- unlist(lapply(ventas.centroamerica, is.numeric))
@@ -170,3 +171,13 @@ compPrinc<-prcomp(vcnum, scale = TRUE)
 summary(compPrinc)
 fviz_pca_var(compPrinc, col.var = "cos2",gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"), repel = TRUE)
 
+h1<-ventas.centroamerica$`Canal de Venta`
+plot(h1, main = "CANAL DE VENTA EN C.A.")
+
+lineas<-table(ventas.centroamerica$Linea)
+h1<-lineas[order(-lineas)]
+head(h1,n=10)
+ps<-c("FAMILY", "CLEAN HOUSE", "COLLECTION EXCLUSUVAS FEM", "COLLECTION EXCLUSUVAS MEN",
+      "SCENTIA NATURALS", "NATURAL PERFECTION", "D'NINOS", "PROMOCIONAL", "ORO LIQUIDO", "TOP SECRET")
+barplot(head(h1, n=10), names.arg = NA, main = "LINEAS MAS PEDIDAS")
+axis(1, at=xx, labels=ps, tick=FALSE, las=2, line=-3, cex.axis=0.5)
