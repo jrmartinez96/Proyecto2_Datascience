bf9fff4fbfb04c30c77860d948bd07e13d1b59be
Pablo Viana <via16091@uvg.edu.gt>
2019-08-31 01:48:21 -0600

    Reestructuracion de codigo

--- testDeImportacion.R
+++ testDeImportacion.R
@@ -1,4 +1,4 @@
-# -------------------------------- Proyecto numero 2 Data science -------------------------------
+# --------------------------------- Proyecto numero 2 Data science -------------------------------
 # ---------------------------------|          SCENTIA             |------------------------------
 # Análisis de Ventas Directas en Centroamérica
 # Catedrática: Lynette García 
@@ -8,23 +8,12 @@
 # Daniel Ixcoy		      16748
 # José Martínez			    15163
 # José Meneses		    	1514
-#####
 
-install.packages("frequency")
 #Librerias
 #Programa para juntar todos los xls en una tabla y exportar un RDATA
 library("readxl")
 library("dplyr")
 library(frequency)
-library(rela)
-library(psych)
-library(FactoMineR)
-library(corrplot)
-library(NbClust)
-library(fpc)
-library(cluster)
-library(fpc) 
-library(factoextra)
 
 # Unión de todos los datos individuales para lograr un conjunto centroamericano
 el_salvador <- read_excel("el_salvador.xlsx", range = "A2:AE6370")
@@ -32,31 +21,34 @@ nicaragua <- read_excel("nicaragua.xlsx", range = "A2:AE6374")
 honduras <- read_excel("honduras.xlsx", range = "A2:AE6685")
 guatemala <- read_excel("guatemala.xlsx",range = "A2:AE7719")
 
-#Arreglo para poder correr rbind
+#en el salvador el nombre de la categoria conca se encuentra en minusculas, se transforma a mayúsculas
+#para poder llevar a cabo sin problemas la función rbind
 names(el_salvador)[names(el_salvador) == "conca"] <- "CONCA"
 
-#Union de todas las tablas
+#Union de todas las tablas para crear un conjunto que represente a centroamérica
 ventas.centroamerica<-rbind(guatemala,honduras,nicaragua,el_salvador)
+
+# ------------------------ Proceso de limpieza de los datos ---------------------------
 
 #Cambio de nombres a columnas
 names(ventas.centroamerica)[names(ventas.centroamerica) == "Pagina..7"] <- "Pagina_cat"
 names(ventas.centroamerica)[names(ventas.centroamerica) == "Pagina..22"] <- "Pagina"
-names(ventas.centroamerica)[names(ventas.centroamerica) == "%..24"] <- "Porcentaje precio"
-names(ventas.centroamerica)[names(ventas.centroamerica) == "%..26"] <- "Porcentaje comision"
+names(ventas.centroamerica)[names(ventas.centroamerica) == "%..24"] <- "Porcentaje"
+names(ventas.centroamerica)[names(ventas.centroamerica) == "%..26"] <- "Porcentaje"
 
 #Arreglo nivel 210811 en categoría Año mes
 ventas.centroamerica$`Año Mes` <- gsub("210811","201811",ventas.centroamerica$`Año Mes`)
 
-#Quitar caracteres de descripcion
+#Quitar caracteres \r|\n de  columna descripcion
 ventas.centroamerica$Descripcion <- gsub("\r|\n","",ventas.centroamerica$Descripcion)
 
-#contingencia FARMA farma
+#transformación en columna contingencia de Farma -> farma
 ventas.centroamerica$Contingencia <- gsub("farma","Farma",ventas.centroamerica$Contingencia)
 
 #Pagina | pagina derecha Pagina derecha, Pagina izquierda pagina izquierda
 ventas.centroamerica$Pagina <- gsub("Pagina derecha","pagina derecha", ventas.centroamerica$Pagina)
 ventas.centroamerica$Pagina <- gsub("Pagina izquierda","pagina izquierda", ventas.centroamerica$Pagina)
-  
+
 #Tipo Precio | intruduccion Introduccion, precio normal Precio Normal
 ventas.centroamerica$`Tipo Precio` <- gsub("Introduccion","introduccion",ventas.centroamerica$`Tipo Precio`)
 ventas.centroamerica$`Tipo Precio` <- gsub("Precio Normal","precio normal",ventas.centroamerica$`Tipo Precio`)
@@ -79,7 +71,7 @@ ventas.centroamerica$Promociones <- gsub("Promocion Precio|Promocion precio","pr
 #Treboles extra | si SI
 ventas.centroamerica$`Treboles extra` <- gsub("SI","si",ventas.centroamerica$`Treboles extra`)
 
-#Cambio de columnas. De tipo char a factor
+#Cambio de columnas de tipo char a factor
 col_names <- c("Año Mes","Producto","Codigo Catalogo","CONCA","Tipo Comision","Pagina_cat","Descripcion","Categoria","Linea","Observaciones","Canal de Venta","Contingencia","Pagina","Tipo Precio","Atributo Neto","Energy Chart","Promociones","Recursos Especiales","Treboles extra")
 ventas.centroamerica$Costo <- as.numeric(ventas.centroamerica$Costo)
 ventas.centroamerica[col_names] <- lapply(ventas.centroamerica[col_names] , factor)
@@ -92,50 +84,55 @@ ventas.centroamerica[col_names] <- lapply(ventas.centroamerica[col_names] , fact
 #Guardamos la unión como un archivo RData
 #save(ventas.centroamerica, file = "ventas_centroamerica.RData")
 
-#-----------------------------------------------------------------
-# ANIO - MES (ventas centroamerica)
+#---------------------- Graficas para entender comportamiento  y estado de los datos -----------
 
+# Gráfico de  ventas por AÑO - MES (ventas centroamerica)
 h1<-table(ventas.centroamerica$`Año Mes`)
 FRECventas2018<-h1[1:12]
 meses<-c("Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago","Sept", "Oct", "Nov","Dic")
 meses2<-c("Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul")
-barplot(FRECventas2018, main = "2018", names.arg = meses)
+#Gráfico de ventas 2018
+barplot(FRECventas2018, main = "VENTAS EN 2018", names.arg = meses)
 FRECventas2019<-h1[13:19]
-barplot(FRECventas2019, main = "2019", names.arg = meses2)
-barplot(h1, main = "2018-2019", names.arg = c(meses, meses2))
+#Gráfico de ventas en 2019
+barplot(FRECventas2019, main = "VENTAS EN 2019", names.arg = meses2)
+#Gráfico de ventas totales, de 2018 a 2019
+barplot(h1, main = "VENTAS TOTALES", names.arg = c(meses, meses2))
 
-# PRODUCTO
-# productos<-table(as.factor(ventas.centroamerica$Producto))
-# str(productos)
-# h1<-productos[order(-productos)]
-# head(h1)
-# xx<-barplot(head(h1, n = 10), names.arg = NA, horiz = FALSE, main = "PRODUCTOS MAS PEDIDOS")
-# head(h1, n = 10)
-# yy<-c(73,73,72,72,71,70,70,70,70,70)
-# text(x = xx, y = yy, labels = yy, pos = 4, cex = 0.9, col = "red")
-# # 4123800056 4123800080 4123800001 4123800030 4123811146 4123660214 4130000001 4130000002 4130000003 4130000004
-# p1<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800056"]
-# p2<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800080"][1]
-# p3<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800001"][1]
-# p4<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800030"][2]
-# p5<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123811146"][1]
-# p6<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123660214"][1]
-# p7<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000001"][1]
-# p8<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000002"][1]
-# p9<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000003"][1]
-# p10<-ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000004"][1]
-# ps<-c(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10)
-# ps
-# p1
-# axis(1, at=xx, labels=ps, tick=FALSE, las=2, line=-11, cex.axis=0.5)
+#Grafico de los 10 productos mas vendidos
+productos<-table(ventas.centroamerica$Producto)
+productos_ordenados<-productos[order(-productos)]
+#Cantidad de cuantos se vendieron de los 10 productos mas vendidos
+yy<-as.character(as.vector(head(productos_ordenados, n = 10)))
+#Ajustamos margenes para el grafico
+par(mar = c(4, 4, 4, 9), xpd = TRUE)
+#Obtenemos los 10 productos mas vendidos
+productos_ordenados <- head(productos_ordenados, n=10)
+#Lo graficamos
+diez_mas_vendidos<-barplot(productos_ordenados,legend.text = yy, args.legend = list(x = "topright", bty = "n", inset=c(-0.15, 0)),horiz = FALSE, main = "PRODUCTOS MAS VENDIDOS", col = c("darkseagreen","darkseagreen1","darkseagreen2","darkseagreen3","darkseagreen4","darkslategray1","darkslategray2","darkslategray3","darkslategray4","grey"))
 
+#Obtenemos la descripción de los 10 productos más vendidos
+p1<-as.character(ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800056"][1])
+p2<-as.character(ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800080"][1])
+p3<-as.character(ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800001"][1])
+p4<-as.character(ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123800030"][2])
+p5<-as.character(ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123811146"][1])
+p6<-as.character(ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4123660214"][1])
+p7<-as.character(ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000001"][1])
+p8<-as.character(ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000002"][1])
+p9<-as.character(ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000003"][1])
+p10<-as.character(ventas.centroamerica$Descripcion[ventas.centroamerica$Producto == "4130000004"][1])
+#Las unimos todas en un vector de descripciones
+ps<-c(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10)
+#Coordinadas del eje x donde poner las descripciones
+xx <- c(0.7,1.9,3.1,4,5.5,6.7,7.9,9.1,10.4,11.5)
+#Las agregamos al gráfico
+text(diez_mas_vendidos, productos_ordenados/2, labels = ps, srt = 90, cex = 0.5)
 
 # CODIGO CATEGORIA
 categorias<-table(ventas.centroamerica$Categoria)
-categorias
-h1<-categorias[order(-categorias)]
-h1
-barplot(h1, main = "CATEGORIAS MAS PEDIDAS")
+cat_ordenadas<-categorias[order(-categorias)]
+barplot(cat_ordenadas)
 
 #Tablas de frecuencia de todas las variables
 freq(ventas.centroamerica$`Año Mes`)
@@ -158,9 +155,6 @@ freq(ventas.centroamerica$Promociones)
 freq(ventas.centroamerica$`Recursos Especiales`)
 freq(ventas.centroamerica$`Treboles extra`)
 
-#Histograma de categoria pronostico
-hist(ventas.centroamerica$Pronostico, breaks=10, col="blue", main="Colored histogram")
-
 # separación de variables numéricas de las categóricas
 num <- unlist(lapply(ventas.centroamerica, is.numeric))
 cat <- unlist(lapply(ventas.centroamerica, is.factor))
@@ -173,9 +167,33 @@ matcorrelacion <- cor(vcnum,use = "pairwise.complete.obs")
 round(matcorrelacion, digits = 6)
 corrplot(matcorrelacion, method="shade", addCoef.col = "black", shade.col=NA, tl.col="black", tl.cex=0.6, tl.srt=45)
 
-# Graficos exploratorios
+# ------------------------- Clusterin ---------------------------------
 
-### PCA ###
+# Método de ward para determinar número adecuado de clusters con K-means
+wss <- (nrow(vcnum)-1)*sum(apply(vcnum,2,var))
+
+for (i in 2:10) 
+  wss[i] <- sum(kmeans(vcnum, centers=i)$withinss)
+
+plot(1:10, wss, type="b", xlab="Number of Clusters",  ylab="Within groups sum of squares")
+
+#Paquete para saber el mejor n?mero de clusters
+nb <- NbClust(vcnum, distance = "euclidean", min.nc = 2,max.nc = 10, method = "complete", index ="all")
+
+# Método K-means para crear los clusters 
+km<-kmeans(vcnum,4)
+vcnum$grupo<-km$cluster
+# gráficos que muestran la ubicación de cada cluster
+plotcluster(vcnum,km$cluster)
+fviz_cluster(km, data = vcnum,geom = "point", ellipse.type = "norm")
+# Calculo de la silueta para K-means
+silkm<-silhouette(km$cluster,dist(vcnum))
+mean(silkm[,3])
+vcnum$grupo<-NULL
+
+# ---------------------------- Reglas de asociación (esto falta) ------------------------
+
+# ---------------------------- Análisis PCA ---------------------
 
 names(vcnum)[12] <- "Porcentaje2"
 vcnum <- vcnum[complete.cases(vcnum),]
@@ -209,29 +227,4 @@ ps<-c("FAMILY", "CLEAN HOUSE", "COLLECTION EXCLUSUVAS FEM", "COLLECTION EXCLUSUV
       "SCENTIA NATURALS", "NATURAL PERFECTION", "D'NINOS", "PROMOCIONAL", "ORO LIQUIDO", "TOP SECRET")
 barplot(head(h1, n=10), names.arg = NA, main = "LINEAS MAS PEDIDAS")
 axis(1, at=xx, labels=ps, tick=FALSE, las=2, line=-3, cex.axis=0.5)
-
-
-### CLUSTERING ###
-
-# Método de ward para determinar número adecuado de clusters con K-means
-wss <- (nrow(vcnum)-1)*sum(apply(vcnum,2,var))
-
-for (i in 2:10) 
-  wss[i] <- sum(kmeans(vcnum, centers=i)$withinss)
-
-plot(1:10, wss, type="b", xlab="Number of Clusters",  ylab="Within groups sum of squares")
-
-#Paquete para saber el mejor n?mero de clusters
-nb <- NbClust(vcnum, distance = "euclidean", min.nc = 2,max.nc = 10, method = "complete", index ="all")
-
-# Método K-means para crear los clusters 
-km<-kmeans(vcnum,4)
-vcnum$grupo<-km$cluster
-# gráficos que muestran la ubicación de cada cluster
-plotcluster(vcnum,km$cluster)
-fviz_cluster(km, data = vcnum,geom = "point", ellipse.type = "norm")
-# Calculo de la silueta para K-means
-silkm<-silhouette(km$cluster,dist(vcnum))
-mean(silkm[,3])
-vcnum$grupo<-NULL
 
